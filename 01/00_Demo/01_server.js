8fc9bff290502df52f27cf6b047879b5b8fed0e9
Zwiebaeckli <jasmin.furler@noseryoung.com>
2024-02-29 10:04:35 +0100

    29.02.2024 Node Modules

--- 01/00_Demo/01_server.js
+++ 01/00_Demo/01_server.js
@@ -1,20 +1,18 @@
 //Add module express
-//??
+const express = require('express');
 //Important: DO NOT FORGET round braces!
-//??
-//??
+const app = express();//??
+const port = 4501;//??
 
-//??
-//??
+app.listen(port);//??
+console.log(`Server running on port ${port}`);//??
 
 
 //GET request
-//??
-//??
-//??
-//??
-//??
-//??
+app.get('/api/v1/students', (req,res) =>{
+    res.send("students api not yet implemented!");
+});
+
 
 //POST request
 //??
